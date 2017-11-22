31133f6eda44b4eb9134974c8b1ce4f292cbc691
Tom <tom@mdashx.com>
2017-11-22 02:03:05 +0000

    Add Flow annotations

--- src/features/findUser/actions.js
+++ src/features/findUser/actions.js
@@ -1,13 +1,22 @@
+// @flow
+
 import { ADD_USER_TO_LIST, UPDATE_USERNAME } from './constants';
 
-export function addUser(username) {
+type AddUserAction = { type: ADD_USER_TO_LIST, username: string };
+type UpdateUsernameAction = { type: UPDATE_USERNAME, username: string };
+
+export type Action =
+  | AddUserAction
+  | UpdateUsernameAction;
+
+export function addUser(username: string): AddUserAction {
   return {
     type: ADD_USER_TO_LIST,
     username,
   };
 }
 
-export function updateUsername(username) {
+export function updateUsername(username: string): UpdateUsernameAction {
   return {
     type: UPDATE_USERNAME,
     username,
--- src/features/findUser/reducer.js
+++ src/features/findUser/reducer.js
@@ -1,14 +1,20 @@
+// @flow
+
 import { fromJS } from 'immutable';
+import type { Map } from 'immutable'
 
 import { ADD_USER_TO_LIST, UPDATE_USERNAME } from './constants';
 import { LOOKUP_USERNAME } from 'features/foundUser/constants';
+import type { Action } from './actions';
+
+type State = Map;
 
 export const initialState = fromJS({
   currentUsername: '',
   usernames: [],
 });
 
-const findUser = (state = initialState, action) => {
+const findUser = (state: State = initialState, action: Action): State => {
   switch (action.type) {
     case UPDATE_USERNAME:
       return state.set('currentUsername', action.username);
